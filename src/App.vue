d94572fa8cf06b4c4c1913950e9cfca8853d52cd
giuseppesciacca <sciaccagi@gmail.com>
2023-04-17 16:19:18 +0200

    [add] header template

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,24 @@ export default {
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <header>
+    <div class="d-flex align-items-center p-3">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/800px-Yu-Gi-Oh%21_%28Logo%29.jpg"
+        alt="">
+      <h1>Yu-Gi-Oh Api</h1>
+    </div>
+  </header>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+header {
+  height: 100px;
+
+  img {
+    max-width: 100%;
+    height: 30px !important;
+    padding-right: 1rem;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import bootstrap from '../node_modules/bootstrap/scss/bootstrap.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
